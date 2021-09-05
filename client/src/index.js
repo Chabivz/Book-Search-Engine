931670dd40f1862c065c647fbdd6dcba926153d0
Chris Abiva <chrisabiva@hotmail.com>
2021-09-04 17:40:44 -0700

    Pulling from repo

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+const Eventine = 'Eventine';
+
 
 ReactDOM.render(
   <React.StrictMode>
